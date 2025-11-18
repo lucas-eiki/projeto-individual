0d33f1dc55ae21480b16cf75bf7a305f36421692
lucas-eiki <lucas.gushiken@sptech.school>
2025-11-18 00:56:57 -0300

    adição(animacao): adiciona nome da categoria da animação

--- site/src/models/animacoesModel.js
+++ site/src/models/animacoesModel.js
@@ -8,7 +8,7 @@ function listar() {
 }
 
 function buscarPorId(id) {
-    var instrucaoSql = `SELECT id, titulo, sinopse, ano_lancamento, imagem, classificacao, id_categoria FROM animacao WHERE id = ${id}`;
+    var instrucaoSql = `SELECT titulo, sinopse, ano_lancamento, imagem, classificacao, categoria.nome AS categoria FROM animacao JOIN categoria ON animacao.id_categoria = categoria.id WHERE animacao.id = ${id}`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
