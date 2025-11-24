9cafe0a51f40873d6c22f8bef3f9fa838ef52476
lucas-eiki <lucas.gushiken@sptech.school>
2025-11-24 01:52:11 -0300

    adição(database): adiciona o arquivo de script de inserts

---
+++ site/src/database/script-inserts.sql
@@ -0,0 +1,19 @@
+INSERT INTO animacao (titulo, sinopse, ano_lancamento, imagem, classificacao, id_categoria) VALUES
+    ()
+
+INSERT INTO categoria (nome) VALUES
+	('2D'),
+	('3D'),
+	('Stop Motion'),
+	('Anime');
+
+INSERT INTO genero (nome) VALUES
+	('Ação'),
+	('Comédia'),
+	('Drama'),
+	('Terror'),
+	('Romance'),
+	('Fantasia'),
+	('Suspense'),
+	('Ficção científica'),
+	('Aventura');
